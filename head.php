9b58119b5ab790e1491455e84d9654453a25d363
Chris Hart <chart@instructure.com>
2018-01-26 10:18:34 -0500

    temp CSS fix for iOS autoprefixer issue

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo $pageTitle ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="description" content="A simple tracker for Portland, Maine, METRO bus lines">
   <meta name="author" content="Christopher Hart">
